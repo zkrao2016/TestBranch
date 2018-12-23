dd33fc5add682c108a04a7f1d1513540c37af016
raozekai <kzr@zheng-chain.com>
2018-12-23 15:01:58 +0800

    master branch

--- app/src/main/java/com/rzk/test/testbranch/MainActivity.java
+++ app/src/main/java/com/rzk/test/testbranch/MainActivity.java
@@ -2,12 +2,25 @@ package com.rzk.test.testbranch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mText = findViewById(R.id.text);
+        mText.setText("主分支");
+
+        onMasterBranch();
+    }
+
+    private void onMasterBranch() {
+        Log.e("", "This is master branch");
     }
 }
